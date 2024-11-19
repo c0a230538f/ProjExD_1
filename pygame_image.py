0ca258114f96456d1d600474ccb6d4cf116f0d0f
YK <c0a230538f@edu.teu.ac.jp>
2024-11-19 16:01:39 +0900

    練習6背景移動

--- pygame_image.py
+++ pygame_image.py
@@ -12,14 +12,17 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg") #背景surfaceを作成（今回は背景）
     tori_img = pg.image.load("fig/3.png") #こうかとんのsurfaceを作成
     tori_img = pg.transform.flip(tori_img, True, False) #surfaceを左右反転　変数(画像（surface）, 左右反転, 上下反転)
-    tmr = 0
+    tmr = 0 #時間カウンター(while1回で1増加)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return #✕ボタンを押したら終了
-        screen.blit(bg_img, [0, 0]) #screen surfaceに背景画像surfaceを貼り付ける（blit→貼り付け）、[横, 縦]で位置指定
+        x = tmr%800
+
+        screen.blit(bg_img, [-x, 0]) #screen surfaceに背景画像surfaceを貼り付ける（blit→貼り付け）、[横, 縦]で位置指定
         screen.blit(tori_img, [300, 200]) #screen surfaceにこうかとんsurfaceを貼り付ける
+
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(200)
 
 
